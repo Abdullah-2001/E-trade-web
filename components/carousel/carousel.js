7daf41b62c7dd38b3fbbabc909ffcfaa686b4671
unknown <abdullah.320409@gmail.com>
2023-01-30 20:30:12 +0500

    commit by abdullah

--- components/carousel/carousel.js
+++ components/carousel/carousel.js
@@ -4,7 +4,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { FiArrowLeft, FiArrowRight } from "react-icons/fi";
 
-export default function MultipleItems() {
+export function MultipleItems() {
 
     function SampleNextArrow(props) {
         const { onClick } = props;
@@ -25,24 +25,28 @@ export default function MultipleItems() {
     }
 
     const arr = [
-        { img: "https://new.axilthemes.com/demo/template/etrade/assets/images/product/product-39.png" },
-        { img: "https://new.axilthemes.com/demo/template/etrade/assets/images/product/product-39.png" },
-        { img: "https://new.axilthemes.com/demo/template/etrade/assets/images/product/product-39.png" },
-        { img: "https://new.axilthemes.com/demo/template/etrade/assets/images/product/product-39.png" },
-        { img: "https://new.axilthemes.com/demo/template/etrade/assets/images/product/product-39.png" },
-        { img: "https://new.axilthemes.com/demo/template/etrade/assets/images/product/product-39.png" },
-        { img: "https://new.axilthemes.com/demo/template/etrade/assets/images/product/product-39.png" },
-        { img: "https://new.axilthemes.com/demo/template/etrade/assets/images/product/product-39.png" },
-        { img: "https://new.axilthemes.com/demo/template/etrade/assets/images/product/product-39.png" },
-        { img: "https://new.axilthemes.com/demo/template/etrade/assets/images/product/product-39.png" },
-        { img: "https://new.axilthemes.com/demo/template/etrade/assets/images/product/product-39.png" },
-        { img: "https://new.axilthemes.com/demo/template/etrade/assets/images/product/product-39.png" },
+        { img: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8&w=1000&q=80" },
+        { img: "https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80" },
+        { img: "https://images.unsplash.com/photo-1563535708875-ddec13a419b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" },
+        { img: "https://images.unsplash.com/photo-1521093470119-a3acdc43374a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" },
+        { img: "https://images.unsplash.com/photo-1600686436197-074b5f921604?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" },
+        { img: "https://images.unsplash.com/photo-1597045566677-8cf032ed6634?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" },
+        { img: "https://images.unsplash.com/photo-1583744946564-b52ac1c389c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" },
+        { img: "https://images.unsplash.com/photo-1622470953794-aa9c70b0fb9d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" },
+        { img: "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" },
+        { img: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" },
+        { img: "https://yt3.ggpht.com/ytc/AMLnZu91BueDfRw_PPeht766E0RdKohGtiwzo89ozw_Jthk=s900-c-k-c0x00ffffff-no-rj" },
+        { img: "https://assets.xboxservices.com/assets/12/7e/127e5830-82cb-4f6c-beef-4f9a0c970430.jpg?n=Consoles-Hub_Content-Placement_Hub-SX_788x444.jpg" },
     ]
 
     const settings = {
         dots: false,
-        slidesToShow: 6,
-        slidesToScroll: 1,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 4,
+        autoplay: true,
+        autoplaySpeed: 3000,
         nextArrow: <SampleNextArrow />,
         prevArrow: <SamplePrevArrow />
     }
@@ -53,8 +57,8 @@ export default function MultipleItems() {
                 {arr.map((v, i) => {
                     return (
                         <>
-                            <img src={v.img} />
-                            <p style={{ fontSize: "18px", fontWeight: "700", marginTop: "15px" }}>Watches</p>
+                            <img className="slider-img" src={v.img} />
+                            <p style={{ fontSize: "18px", fontWeight: "500", marginTop: "15px" }}>Category</p>
                         </>
                     )
                 })}
--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,17 @@ import { Col, Container, Row } from 'react-bootstrap'
 import Button from '../components/button/button'
 import styles from '../styles/Home.module.css'
 import { BsCart } from 'react-icons/bs'
-import { FaEye, FaFire, FaHeadphones, FaHeart, FaSalesforce, FaShoppingBag, FaTag, FaTags } from 'react-icons/fa'
-import Carousel from '../components/carousel/carousel'
+import { FaEye, FaFire, FaGamepad, FaHeadphones, FaHeart, FaSalesforce, FaShoppingBag, FaTag, FaTags } from 'react-icons/fa'
+import { MultipleItems, FadeSlider } from '../components/carousel/carousel'
 import Timer from '../components/timer/timer'
 import Card from '../components/card/card'
 import ModalComp from '../components/modal/modal'
-import { useState } from 'react'
+import sale from '../assets/man.svg'
+import { useEffect, useState } from 'react'
+import products from '../data.json'
 
 export default function Home() {
 
-  const products = [
-    { image: "https://new.axilthemes.com/demo/template/etrade/assets/images/product/poster/poster-03.png", title: "Product", price: 10.87 },
-    { image: "https://new.axilthemes.com/demo/template/etrade/assets/images/product/poster/poster-03.png", title: "Product", price: 10.87 },
-    { image: "https://new.axilthemes.com/demo/template/etrade/assets/images/product/poster/poster-03.png", title: "Product", price: 10.87 },
-    { image: "https://new.axilthemes.com/demo/template/etrade/assets/images/product/poster/poster-03.png", title: "Product", price: 10.87 },
-    { image: "https://new.axilthemes.com/demo/template/etrade/assets/images/product/poster/poster-03.png", title: "Product", price: 10.87 },
-    { image: "https://new.axilthemes.com/demo/template/etrade/assets/images/product/poster/poster-03.png", title: "Product", price: 10.87 },
-    { image: "https://new.axilthemes.com/demo/template/etrade/assets/images/product/poster/poster-03.png", title: "Product", price: 10.87 },
-    { image: "https://new.axilthemes.com/demo/template/etrade/assets/images/product/poster/poster-03.png", title: "Product", price: 10.87 },
-  ]
   const [modalShow, setModalShow] = useState(false);
 
   return (
@@ -58,6 +50,7 @@ export default function Home() {
               </Col>
               <Col md={6}>
                 <img className='img-fluid' src='https://new.axilthemes.com/demo/template/etrade/assets/images/product/product-38.png' />
+                {/* <img className='img-fluid img-1' src='https://new.axilthemes.com/demo/template/etrade/assets/images/product/product-39.png' /> */}
               </Col>
             </Row>
           </Container>
@@ -72,7 +65,7 @@ export default function Home() {
             <p className={styles.categories_title}>Categories</p>
           </div>
           <h1>Browse by Category</h1>
-          <Carousel />
+          <MultipleItems />
         </Container>
       </section>
       <section className={styles.sale_section_wrapper}>
@@ -82,11 +75,11 @@ export default function Home() {
               <Col md={6}>
                 <div className={styles.sale_title_wrapper}>
                   <span className={styles.headphone_icon_circle}>
-                    <FaHeadphones />
+                    <FaGamepad />
                   </span>
                   <p className={styles.sale_title}>Don’t Miss!!</p>
                 </div>
-                <h1>Enhance Your Music Experience</h1>
+                <h1>Enhance Your Gaming Experience</h1>
                 <Timer />
                 <Button
                   width="200px"
@@ -100,7 +93,24 @@ export default function Home() {
                 </Button>
               </Col>
               <Col md={6}>
-                <img src='https://new.axilthemes.com/demo/template/etrade/assets/images/product/poster/poster-03.png' className='img-fluid' />
+                <div id="wifi-loader">
+                  <svg className="circle-outer" viewBox="0 0 86 86">
+                    <circle className="back" cx="43" cy="43" r="40"></circle>
+                    <circle className="front" cx="43" cy="43" r="40"></circle>
+                    <circle className="new" cx="43" cy="43" r="40"></circle>
+                  </svg>
+                  <svg className="circle-middle" viewBox="0 0 60 60">
+                    <circle className="back" cx="30" cy="30" r="27"></circle>
+                    <circle className="front" cx="30" cy="30" r="27"></circle>
+                  </svg>
+                  <svg className="circle-inner" viewBox="0 0 34 34">
+                    <circle className="back" cx="17" cy="17" r="14"></circle>
+                    <circle className="front" cx="17" cy="17" r="14"></circle>
+                  </svg>
+                </div>
+                <div>
+                <img src='https://cdn.shopify.com/s/files/1/0636/8837/6542/products/049-024-WH_XBX_AfterglowWave_BLACK_ECOM-FRONT.Camera1_f45bec4e-98c2-45d9-94bd-9d593586d68a.png?v=1664391713' className='img-fluid' />
+                </div>
               </Col>
             </Row>
           </div>
@@ -124,18 +134,7 @@ export default function Home() {
               return (
                 <Col md={3}>
                   <Card className="products_card">
-                    <div className="figure">
-                      <a href="https://www.example.com/">
-                        <img
-                          className="Sirv image-main"
-                          src="https://static-01.daraz.pk/p/fd7e4ccdd08f089b0a9f55ca53eed1fe.jpg"
-                        />
-                        <img
-                          className="Sirv image-hover"
-                          src="https://pakistanstore.pk/wp-content/uploads/2022/09/PS4-Dualshock-Wireless-Game-Controller.jpg"
-                        />
-                      </a>
-                    </div>
+                    <img src={item.image} alt="" />
                     <div className="title-price-container">
                       <p>{item.title}</p>
                       <h5>$ {item.price}</h5>
@@ -156,6 +155,15 @@ export default function Home() {
           </Row>
         </Container>
         <ModalComp show={modalShow} onHide={() => setModalShow(false)} title="This is title" body="This is body" />
+      </section>
+      <section className={styles.sale_section_wrapper}>
+        <Container>
+          <Row className="align-items-center">
+            <Col md={12} lg={12}>
+              <img width={500} src={sale.src} alt='' />
+            </Col>
+          </Row>
+        </Container>
       </section>
     </>
   )
